1a9aefa10c7aa0d0f6a5d37d7aee113c03e0f786
Ashish K Pathak <ashishkpathak.connect@gmail.com>
2024-09-14 14:18:53 +0530

    Bootstrap manifests, code, CRD and Update API.
    - Added initial manifests, CRDs, and code structure for the project.
    
    - Updated API type Spec and Status fields to reflect the latest design changes.

---
+++ api/v1/certificate_types.go
@@ -0,0 +1,71 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// SecretReference represents a Secret Reference.
+type SecretReference struct {
+
+	// +kubebuilder:validation:Pattern=`^[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`
+
+	// The name of the Secret in which certificate is stored.
+	// This should be a valid DNS Subdomain Name. It cannot be empty.
+	Name string `json:"name"`
+}
+
+// CertificateSpec defines the desired state of Certificate
+type CertificateSpec struct {
+
+	// +kubebuilder:validation:Pattern=`^[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`
+
+	// The DNS name for which the certificate should be issued.
+	// This should be a valid DNS Subdomain Name. It cannot be empty.
+	DNSName string `json:"dnsName"`
+
+	// +kubebuilder:validation:Pattern=`^\d+d$`
+
+	// The time until the certificate expires.
+	// Accepts sequence of 1 or more digits followed by the letter 'd' which indicates days.
+	Validity string `json:"validity"`
+
+	// A reference to the Secret object in which certificate is stored.
+	SecretRef SecretReference `json:"secretRef"`
+}
+
+// CertificateStatus defines the observed state of Certificate
+type CertificateStatus struct {
+
+	// Condition contains details for one aspect of the current state of this API Resource.
+	// +optional
+	Condition metav1.Condition `json:"condition"`
+
+	// Name of the Secret object which is created.
+	// +optional
+	SecretName string `json:"secretName"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Certificate is the Schema for the certificates API
+type Certificate struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CertificateSpec   `json:"spec,omitempty"`
+	Status CertificateStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// CertificateList contains a list of Certificate
+type CertificateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Certificate `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Certificate{}, &CertificateList{})
+}
